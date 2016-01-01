8e1032a99c29bde55bc9f583649f7596925b97a3
utahkaA <utahka.akiba@gmail.com>
2016-01-01 16:55:22 +0900

    update music bottle firmware
    - added the .gitignore
    - modified the README.md

--- main.cpp
+++ main.cpp
@@ -1,41 +1,65 @@
+// -------------------------------------------------------------------------------
+// Name:           music_bottle/main.cpp
+// Author:         @utahkaA (Twitter, GitHub, and so on...)
+// Created:        Nov 18th, 2015
+// Last Date:      Dec 30th, 2015
+// Note:           xxxxx
+// -------------------------------------------------------------------------------
 #include "mbed.h"
 #include "SDFileSystem.h"
 #include "wave_player.h"
+#include <string>
 
-#define C 261.626
-#define D 293.665
-#define E 329.628
-#define F 349.228
-#define G 391.995
-#define A 440.000
-#define B 493.883
+const int MAX = 65535;
+const int THRESHOLD_1 = 40000;  // Threshold
+const int THRESHOLD_2 = 20000;
 
-int main(void) {    
-    AnalogIn press(p20);
-    AnalogOut speaker(p18);
-    // PwmOut speaker(p21);
-    SDFileSystem sd(p5, p6, p7, p8, "sd");
-    wave_player waver(&speaker);
-    
-    // float mm[]={C, D, E, F, G, A, B, C*2};
-        
-    // mkdir("/sd/mydir", 0777);
+AnalogOut speaker(p18);
+AnalogIn press(p20);
+SDFileSystem sd(p5, p6, p7, p8, "sd");
+wave_player waver(&speaker);
+Ticker flipper;
 
-    FILE *wav_file = fopen("/sd/hogehoge.wav", "r");
+std::string file_name = "/sd/hogehoge.wav";
+bool playing;
+FILE *wav_file;
+
+void wav_open(string wav_name) {
+    wav_file = fopen(wav_name.c_str(), "r");
     if (wav_file == NULL) {
-       error("Could not open file for write\n");
+        error("Could not open wav file for read\n");
     }
-    waver.play(wav_file);
-    fclose(wav_file);
-       
-    while(1) {
-        printf("press: %d\n\r", press.read_u16());
-        // for (int i = 0; i < sizeof(mm); i++) {
-        //     speaker.period(1.0/mm[i]);
-        //     speaker.write(0.5f);
-        //     wait(0.5f);
-        //     speaker.write(0.0f);
-        // }
-        wait(1);
+}
+
+void change_music() {
+    printf("press: %d\n\r", press.read_u16());
+    int val = press.read_u16();
+    if (val == MAX && playing) {
+        playing = false;
+        fclose(wav_file);
+    }
+
+    if (THRESHOLD_1 <= val && val < MAX && !playing) {
+        wav_open("/sd/hogehoge.wav");
+        playing = true;
+    }
+
+    if (THRESHOLD_2 <= val && val < THRESHOLD_1 && !playing) {
+        wav_open("/sd/secret-iii-exposure.wav");
+        playing = true;
+    }
+
+    if (val < THRESHOLD_2 && !playing) {
+        wav_open("/sd/re-paradox.wav");
+        playing = true;
+    }
+}
+
+int main(void) {
+    flipper.attach(&change_music, 0.5);
+    while (1) {
+        while (playing) {
+            waver.play(wav_file);
+        }
    }
 }
